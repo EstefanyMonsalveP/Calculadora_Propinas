a4e259d98c9e125556e2f458365c5715e3e2e0ef
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-08-10 16:47:23 -0500

    feat(MenuItem): Crear  componente MenuItem para cargar todos los items de la db e integrarlo en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 function App() {
 console.log(menuItems)
@@ -8,8 +9,18 @@ console.log(menuItems)
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menú</h2>
+        <div className="space-y-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+            <MenuItem
+            key={item.id}
+            item={item}
+            />
+          ))}
+          </div>
+          
         </div>
 
         <div>
